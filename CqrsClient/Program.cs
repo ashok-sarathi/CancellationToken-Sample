212afbf0fda4be39b7dad7f4c045ab7c1f18571b
ashok-sarathi <ashoksarathi004@gmail.com>
2025-02-06 19:36:13 +0530

    init app

---
+++ CqrsClient/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CqrsClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("https://localhost:7126")
+            };
+
+            var data1 = client.GetAsync("api/Product").Result;
+            //Task.Delay(10000).Wait();
+            //CancellationTokenSource cts = new CancellationTokenSource();
+            //cts.CancelAfter(3000);
+            //var data2 = await client.GetAsync("api/Product", cts.Token);
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ CqrsUsingMediatRApi/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using CqrsUsingMediatRApi.Operations.Products.Handlers.Create;
+using CqrsUsingMediatRApi.Operations.Products.Handlers.Get;
+using CqrsUsingMediatRApi.Operations.Products.Handlers.List;
+using CqrsUsingMediatRApi.Operations.Products.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CqrsUsingMediatRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(ISender sender) : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public async Task<IEnumerable<ProductModel>> Get(CancellationToken cancellationToken)
+        {
+            //while(cancellationToken.IsCancellationRequested)
+            //{
+            //    Results.StatusCode(499);
+            //    return [];
+            //}
+            //await Task.Delay(10000);
+            return await sender.Send(new ListProductsQueryRequest(), cancellationToken);
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public async Task<ProductModel> Get(Guid id, CancellationToken cancellationToken)
+        {
+            return await sender.Send(new GetProductQueryRequest() { Id = id }, cancellationToken);
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<Guid> Post([FromBody] ProductModel model, CancellationToken cancellationToken)
+        {
+            return await sender.Send(new CreateProductCommendRequest() { Name = model.Name, Description = model.Description, Price = model.Price }, cancellationToken);
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut]
+        public async Task Put([FromBody] ProductModel model, CancellationToken cancellationToken)
+        {
+            await sender.Send(new UpdateProductCommendRequest() { Id = model.Id, Name = model.Name, Description = model.Description, Price = model.Price }, cancellationToken);
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public async Task Delete(Guid id, CancellationToken cancellationToken)
+        {
+            await sender.Send(new DeleteProductCommendRequest() { Id = id }, cancellationToken);
+        }
+    }
+}
